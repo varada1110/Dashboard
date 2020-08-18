422481cba2c13433ce81bda1e8ac17031a76f634
Varada M <58105997+varada1110@users.noreply.github.com>
2020-08-18 17:34:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function Fun(){
+  var x=document.getElementById("k");
+  
+  if (x.style.display === "block") 
+  {
+    x.style.display = "block";
+  } 
+  else
+  {
+    
+    x.style.display = "none";
+    
+  }
+}
+function Func(){
+  var y=document.getElementById("k");
+  if (y.style.display === "none") 
+  {
+    y.style.display = "block";
+  } 
+}
